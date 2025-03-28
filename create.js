2ee45282a35d595a2b9ad9768fd3ceca08659bf9
Seyfo13 <162480519+Seyfo13@users.noreply.github.com>
2025-03-28 21:29:27 +0400

    qeydiyyat ugurlu mesaji

--- create.js
+++ create.js
@@ -2,44 +2,61 @@ let form = document.querySelector("form");
 let button = document.querySelector("#btn1");
 let inputs = document.querySelectorAll("input");
 
-inputs.forEach(input=>{
-    input.addEventListener("keyup",()=>{
+// Uğur mesajını yarat və formun üstünə əlavə et
+let successMessage = document.createElement("p");
+successMessage.textContent = "Qeydiyyat uğurlu!";
+successMessage.style.color = "green";
+successMessage.style.textAlign = "center";
+successMessage.style.fontSize = "18px";
+successMessage.style.display = "none";
+form.prepend(successMessage);
+
+inputs.forEach(input => {
+    input.addEventListener("keyup", () => {
         let trueMessage = input.nextElementSibling;
-      
+
         if (input.checkValidity()) {
             trueMessage.style.display = "block";
-        
-        }else{
+        } else {
             trueMessage.style.display = "none";
-          
         }
-    })
-})
+    });
+});
 
-button.addEventListener("click",(event)=>{
+button.addEventListener("click", (event) => {
     event.preventDefault();
-    if(form.checkValidity()){
+    if (form.checkValidity()) {
         addNewUser();
-        form.reset(); 
-    }else{
+        
+        // Inputların içini tam təmizlə
+        inputs.forEach(input => {
+            input.value = "";
+        });
+
+        // Uğur mesajını göstər və 3 saniyə sonra gizlət
+        successMessage.style.display = "block";
+        setTimeout(() => {
+            successMessage.style.display = "none";
+        }, 3000);
+    } else {
         console.log("Form valid deyil");
     }
-})
+});
 
 function checkUserFromLocalStorage() {
     let users = JSON.parse(localStorage.getItem("users")) || [];
-    return users
+    return users;
 }
 
 function addNewUser() {
     let newUser = {};
     inputs.forEach(input => {
-        newUser[input.name] = input.value
+        newUser[input.name] = input.value;
     });
     let users = checkUserFromLocalStorage();
     users.push(newUser);
     localStorage.setItem("users", JSON.stringify(users));
-    console.log("User Elave edildi" , newUser);
+    console.log("User Elave edildi", newUser);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
